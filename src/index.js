f1fd7600779d1b804d84e741d41fe74f3e019e63
shutsugan <muhammad@nextmedia.ma>
2019-06-02 16:10:32 +0000

    chore (app): get all cards and remove by clicking

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import Flip from './flip.js';
 
-console.log(Flip);
+const removeCard = card => {
+	card.addEventListener('click', _ => card.parentNode.removeChild(card));
+};
+
+const removeCards = _ => {
+	const cards = Array.from(document.querySelectorAll('.card'));
+	cards.forEach(card => removeCard(card));
+};
+
+addEventListener('load', removeCards);
