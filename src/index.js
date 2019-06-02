b78f77c569632f1b54746d18a0e0f58aab2c0c7c
shutsugan <muhammad@nextmedia.ma>
2019-06-02 17:33:59 +0000

    refactor (app): clean the code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import Flip from './flip.js';
 
-const flipCards = (cards, card, cb) => {
-	const flip = new Flip();
-	const cardsToFlip = cards.filter(c => c !== card);
-	
-	flip.read(cards);
+let cards = Array.from(document.querySelectorAll('.card'));
+cards.forEach(card => {
+	card.addEventListener('click', _ => {
+		const flip = new Flip();
+		flip.read(cards);
+		flip.remove([card]);
 
-	cb();
-
-	flip.play(cardsToFlip);
-};
-
-const removeCard = (cards, card) => {
-	card.addEventListener('click', _ => flipCards(cards, card, _ => {
-		card.parentNode.removeChild(card);
-	}));
-};
-
-const removeCards = _ => {
-	const cards = Array.from(document.querySelectorAll('.card'));
-	cards.forEach(card => removeCard(cards, card));
-};
-
-addEventListener('load', removeCards);
+		cards = cards.filter(c => c !== card);
+		flip.play(cards);
+	});
+});
