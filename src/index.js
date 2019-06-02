b5fbd6aab3060d8db97f75db60759250266f807f
shutsugan <muhammad@nextmedia.ma>
2019-06-02 16:40:39 +0000

    chore (app): remove and flip the card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import Flip from './flip.js';
 
-const removeCard = card => {
-	card.addEventListener('click', _ => card.parentNode.removeChild(card));
+const flipCards = (cards, card, cb) => {
+	const flip = new Flip();
+	const cardsToFlip = cards.filter(c => c !== card);
+	
+	flip.read(cards);
+
+	cb();
+
+	flip.play(cardsToFlip);
+};
+
+const removeCard = (cards, card) => {
+	card.addEventListener('click', _ => flipCards(cards, card, _ => {
+		card.parentNode.removeChild(card);
+	}));
 };
 
 const removeCards = _ => {
 	const cards = Array.from(document.querySelectorAll('.card'));
-	cards.forEach(card => removeCard(card));
+	cards.forEach(card => removeCard(cards, card));
 };
 
 addEventListener('load', removeCards);
