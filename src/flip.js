10aadfe3e00352198264f53564fd56b3e1430313
shutsugan <muhammad@nextmedia.ma>
2019-06-02 16:42:11 +0000

    feat (flip): add read and play methods
    read elements init position
    when played get the new and old position
    and get the final position

--- src/flip.js
+++ src/flip.js
@@ -1,3 +1,42 @@
 export default class Flip {
-	constructor() {}
+	constructor() {
+		this._duration = 150;
+		this._positions = {}
+	}
+
+	/**
+	 * Memorize the elements position
+	 *
+	 * @param {HTMLElement []} elements
+	 */
+	read(elements) {
+		elements.forEach(element => {
+			const id = element.dataset.id;
+			this._positions[id] = element.getBoundingClientRect();
+		});
+	}
+
+	/**
+	 * Animate the element toward the new position
+	 *
+	 * @param {HTMLElement []} elements
+	 */
+	play(elements) {
+		elements.forEach(element => {
+			const id = element.dataset.id;
+
+			const newPosition = element.getBoundingClientRect();
+			const oldPosition = this._positions[id];
+
+			const deltaX = oldPosition.x - newPosition.x;
+			const deltaY = oldPosition.y - newPosition.y;
+			const deltaWidth = oldPosition.width / newPosition.width;
+			const deltaHeight = oldPosition.height / newPosition.height;
+
+			element.style.transform = `
+				translate(${deltaX}px, ${deltaY}px)
+				scale(${deltaWidth}, ${deltaHeight})
+			`;
+		});
+	}
 };
