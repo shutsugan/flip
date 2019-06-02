786c6d512c613309dcb28c6d2237c81214cd165c
shutsugan <muhammad@nextmedia.ma>
2019-06-02 17:33:37 +0000

    feat (flip): add remove method
    remove element with animation
    refactor the code

--- src/flip.js
+++ src/flip.js
@@ -1,6 +1,6 @@
 export default class Flip {
 	constructor() {
-		this._duration = 150;
+		this._duration = 500;
 		this._easing = 'ease-in-out';
 		this._fill = 'both';
 		this._positions = {}
@@ -25,24 +25,87 @@ export default class Flip {
 	 */
 	play(elements) {
 		elements.forEach(element => {
-			const id = element.dataset.id;
+			const {
+				deltaX,
+				deltaY,
+				deltaWidth,
+				deltaHeight
+			} = this.getElementDetails(element);
 
-			const newPosition = element.getBoundingClientRect();
-			const oldPosition = this._positions[id];
-
-			const deltaX = oldPosition.x - newPosition.x;
-			const deltaY = oldPosition.y - newPosition.y;
-			const deltaWidth = oldPosition.width / newPosition.width;
-			const deltaHeight = oldPosition.height / newPosition.height;
-
-			element.animate([
+			const steps = [
 				{transform: `translate(${deltaX}px, ${deltaY}px) scale(${deltaWidth}, ${deltaHeight})`},
 				{transform: 'none'}
-			], {
-				duration: this._duration,
-				easing: this._easing,
-				fill: this._fill
-			});
+			];
+
+			this.setAnimation(element, steps);
+		});
+	}
+
+	/**
+	 * Remove elements with animation
+	 *
+	 * @param {HTMLElement []} elements
+	 */
+	remove(elements) {
+		elements.forEach(element => element.parentNode.appendChild(element));
+		elements.forEach(element => {
+			const {
+				deltaX,
+				deltaY,
+				deltaWidth,
+				deltaHeight
+			} = this.getElementDetails(element);
+
+			const steps = [
+				{
+					transform: `translate(${deltaX}px, ${deltaY}px)`,
+					opacity: 1
+				},
+				{
+					transform: `translate(${deltaX}px, ${deltaY - 30}px)`,
+					opacity: 0
+				}
+			];
+
+			this.setAnimation(element, steps);
+			window.setTimeout(_ => element.remove(), this._duration);
+		});
+	}
+
+	/**
+	 * Get element position and dimentions
+	 *
+	 * @param {HTMLElement} element
+	 */
+	getElementDetails(element) {
+		const id = element.dataset.id;
+
+		const newPosition = element.getBoundingClientRect();
+		const oldPosition = this._positions[id];
+
+		const deltaX = oldPosition.x - newPosition.x;
+		const deltaY = oldPosition.y - newPosition.y;
+		const deltaWidth = oldPosition.width / newPosition.width;
+		const deltaHeight = oldPosition.height / newPosition.height;
+
+		return {
+			deltaX,
+			deltaY,
+			deltaWidth,
+			deltaHeight
+		};
+	}
+
+	/**
+	 * Set element animation
+	 *
+	 * @param {HTMLElement} element
+	 */
+	setAnimation(element, steps) {
+		element.animate(steps, {
+			duration: this._duration,
+			easing: this._easing,
+			fill: this._fill
 		});
 	}
 };
